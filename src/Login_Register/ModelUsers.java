eb34ca458e07faccfe3a0b14a94df4a598f11021
russell <russell@66.71.41.22>
2018-09-28 15:08:54 -0400

    Changed Login_Register model class to reflect changes to our data

--- src/Login_Register/ModelUsers.java
+++ src/Login_Register/ModelUsers.java
@@ -15,50 +15,14 @@ import java.io.*;
  * @author russell
  */
 public class ModelUsers {
-    private String url;
-    private String user;
-    private String password;
-        
-    private String query;
+    private LinkedList<User> users;
 
-    public ModelUsers(String url, String user, String password, String query) {
-        this.url = "jdbc:mysql://localhost:3306/testdb?useSSL=false";
-        this.user = "testuser";
-        this.password = "test623";
-        
-        this.query = "SELECT VERSION()";
+    public ModelUsers() {
+        users = new LinkedList<>();
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
+    public LinkedList<User> getUsers() {
+        return users;
     }
     
 }
